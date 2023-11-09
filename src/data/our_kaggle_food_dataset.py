47691f496232242ae786ce5c486e00c011aa3216
KarlUlbaek <karl.ulbaek@hotmail.com>
2023-11-09 08:32:35 +0100

    almost working baseline using pretrained encoders (minor fix)

---
+++ src/data/our_kaggle_food_dataset.py
@@ -0,0 +1,85 @@
+import os
+import pandas as pd
+import torch
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+
+class KaggleFoodDataset(Dataset):
+    def __init__(self, csv_file, image_dir, transform=transforms.ToTensor()):
+        """
+        Args:
+            csv_file (string): Path to the csv file with recipe and image mappings.
+            image_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied on the image.
+        """
+        self.food_data = pd.read_csv(csv_file).dropna()
+        self.image_dir = image_dir
+        self.transform = transform
+        #self.all_data = torch.load(os.path.join(image_dir, "all_data_float16.pt"))
+
+
+    def __len__(self):
+        return len(self.food_data)
+
+    # def __getitem__(self, idx):
+    #     #img_name = os.path.join(self.image_dir, self.food_data.iloc[idx, 4] + ".jpg")
+    #     #image = Image.open(img_name).convert('RGB')
+    #     #image = self.transform(image)
+    #     #image = transforms.ToTensor()(image)
+    #     #image = torch.load(img_name).to(torch.float16)
+    #     image = self.all_data[idx]
+    #     recipe_title = self.food_data.iloc[idx, 1]
+    #
+    #     return image, recipe_title
+
+    def __getitem__(self, idx):
+        img_name = os.path.join(self.image_dir, self.food_data.iloc[idx, 4] + ".jpg")
+        image = Image.open(img_name).convert('RGB')
+        # Resize images to ensure same dimensions, 224 because many models are pretrained on that
+        #image = transforms.Resize((224,224))(image)
+
+        image = self.transform(image)
+
+        #torch.save(image, os.path.join(self.image_dir, self.food_data.iloc[idx, 4] + ".pt"))
+        # recipe = {
+        #     'title': [self.food_data.iloc[idx, 1]],
+        #     'ingredients': [self.food_data.iloc[idx, 2]],
+        #     'instructions': [self.food_data.iloc[idx, 3]]
+        # }
+        recipe_title = self.food_data.iloc[idx, 1]
+        #image = transforms.ToTensor()(image).to(torch.float32)
+
+
+        return image, recipe_title
+
+
+def test():
+    # Create the dataset and dataloader using the new class
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    food_dir = os.path.join(current_dir,'..','..','data','processed','KaggleFoodDataset')
+    csv_file_path = os.path.join(food_dir, 'data.csv')
+    image_dir = os.path.join(food_dir,'images')
+
+    batch_size = 1000
+    food_dataset_batched = KaggleFoodDataset(csv_file=csv_file_path, image_dir=image_dir)
+    food_dataloader_batched = DataLoader(food_dataset_batched, batch_size=batch_size, shuffle=False)
+
+    # Test the DataLoader by fetching one batch of data
+    #batch_images, batch_recipe_titles = next(iter(food_dataloader_batched))
+    import tqdm
+    data = []
+    for batch_images, _ in tqdm.tqdm(food_dataloader_batched):
+        data.append(batch_images)
+
+    # Show shapes for verification
+    # print(f'Batch size: {batch_size}')
+    # print(f'Image batch shape: {batch_images.shape}')
+    # print(f'Type of image batch: {type(batch_images)}')
+    # print(f'Recipe titles: {batch_recipe_titles}')
+    # print(f'Type of recipe titles: {type(batch_recipe_titles)}')
+    # print(f'Type of recipe titles element: {type(batch_recipe_titles[0])}')
+
+
+if __name__ == '__main__':
+    test()
--- src/models/our_model.py
+++ src/models/our_model.py
@@ -1,7 +1,29 @@
 import torch
 from torch import nn
 import torch.nn.functional as f
+import tqdm
+import matplotlib.pyplot as plt
 
+class ResNet_wrapper(torch.nn.Module):
+    def __init__(self, model, latent_dim=768, d="cuda"):
+        super().__init__()
+        self.d = d
+        self.latent_dim = latent_dim
+        self.model = model
+        self.activation = torch.nn.ReLU()
+
+        if self.model.name_or_path.endswith(str(50)):
+            self.fc = torch.nn.Linear(2048, latent_dim)
+
+        elif self.model.name_or_path.endswith(str(18)):
+            self.fc = torch.nn.Linear(512, latent_dim)
+
+    def forward(self, images):
+        output = self.model(images)
+        output = output.pooler_output.squeeze()
+        out = self.fc(self.activation(output))
+
+        return out
 
 class ViT_wrapper(torch.nn.Module):
     def __init__(self, ViTModel, ViTImageProcessor, latent_dim=768, d="cuda"):
@@ -10,13 +32,15 @@ class ViT_wrapper(torch.nn.Module):
         self.latent_dim = latent_dim
         self.ViTModel = ViTModel
         self.ViTImageProcessor = ViTImageProcessor
+        self.activation = torch.nn.GELU()
         self.fc = torch.nn.Linear(latent_dim, latent_dim)
 
     def forward(self, images):
-        preprocessed = self.ViTImageProcessor(images=images, return_tensors="pt").to(self.d)
-        output = self.ViTModel(**preprocessed)
+        #preprocessed = self.ViTImageProcessor(images=images, return_tensors="pt", do_rescale=False).to(self.d)
+        #output = self.ViTModel(**preprocessed)
+        output = self.ViTModel(images)
         cls_token = output.pooler_output
-        out = self.fc(cls_token)
+        out = self.fc(self.activation(cls_token))
 
         return out
 
@@ -28,6 +52,7 @@ class Bert_wrapper(torch.nn.Module):
         self.latent_dim = latent_dim
         self.BertModel = BertModel
         self.BertTokenizer = BertTokenizer
+        self.activation = torch.nn.GELU()
         self.fc = torch.nn.Linear(latent_dim, latent_dim)
         self.t = torch.nn.Parameter(torch.tensor([0.07])) # init value from clip paper
 
@@ -40,7 +65,7 @@ class Bert_wrapper(torch.nn.Module):
 
         output = self.BertModel(**preprocessed)
         cls_token = output.pooler_output
-        out = self.fc(cls_token)
+        out = self.fc(self.activation(cls_token))
 
         return out
 
@@ -51,91 +76,104 @@ if __name__ == "__main__":
     #     torch.set_default_device("cuda")
     # else:
     #     torch.set_default_device("cpu")
-    d = "cpu"
+    d = "cuda"
 
     from torch.utils.data import DataLoader
-    from src.data.kaggle_food_dataset import *
+    from src.data.our_kaggle_food_dataset import *
     current_dir = os.path.dirname(os.path.abspath(__file__))
     food_dir = os.path.join(current_dir,'..','..','data','processed','KaggleFoodDataset')
     csv_file_path = os.path.join(food_dir, 'data.csv')
     image_dir = os.path.join(food_dir,'images')
 
-    batch_size = 3
-    food_dataset_batched = KaggleFoodDataset(csv_file=csv_file_path, image_dir=image_dir)
+    preprocess = transforms.Compose([
+        #transforms.Resize((224,224)),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    batch_size = 44 #200 is max for resnet18, 80 is max for resnet50, 44 is max for ViT
+    food_dataset_batched = KaggleFoodDataset(csv_file=csv_file_path, image_dir=image_dir, transform=preprocess)
     food_dataloader_batched = DataLoader(food_dataset_batched, batch_size=batch_size, shuffle=True)
 
     batch_images, batch_recipe_titles = next(iter(food_dataloader_batched))
 
 
-    from transformers import BertTokenizer, BertModel, AutoImageProcessor, ViTMAEModel, ViTImageProcessor, ViTModel
+    from transformers import BertTokenizer, BertModel, ViTModel, AutoImageProcessor
 
-
-    #ViTImageProcessor_ = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224-in21k')
-    #ViTModel_ = ViTModel.from_pretrained('google/vit-base-patch16-224-in21k')
-    #vision_model = ViT_wrapper(ViTModel_, ViTImageProcessor_, d=d).to(d)
-    ViTImageProcessor_ = AutoImageProcessor.from_pretrained("facebook/vit-mae-base")
-    ViTModel_ = ViTMAEModel.from_pretrained("facebook/vit-mae-base")
-    vision_model = ViT_wrapper(ViTModel_, ViTImageProcessor_, d=d).to(d)
-    #vision_model(batch_images)
-
+    # ViTImageProcessor_ = AutoImageProcessor.from_pretrained("facebook/vit-mae-base")# uses same as resnet!
+    ViTModel_ = ViTModel.from_pretrained("facebook/vit-mae-base")
+    vision_model = ViT_wrapper(ViTModel_, None, d=d).to(d)
+    # #vision_model(batch_images)
 
     BertModel_ = BertModel.from_pretrained("bert-base-uncased")
     BertTokenizer_ = BertTokenizer.from_pretrained('bert-base-uncased')
     text_model = Bert_wrapper(BertModel_, BertTokenizer_, d=d).to(d)
-    #batch_recipe_titles = list(batch_recipe_titles[0])
-    #text_model(batch_recipe_titles)
 
+    # from transformers import ResNetModel
+    # ResNet = ResNetModel.from_pretrained("microsoft/resnet-50")
+    # vision_model = ResNet_wrapper(ResNet, d=d).to(d)
 
-    print(torch.empty(3, dtype=torch.long).random_(5))
-    import tqdm
-
-    labels = torch.arange(batch_size).to(d)
     loss_fn = torch.nn.CrossEntropyLoss()
-    lr = 1e-4
+    lr = 0.00003 # 0.00003 for Vit (since smaller batchsize) 0.0001 for resnet
     opt_vision = torch.optim.AdamW(lr=lr, params=vision_model.parameters())
     opt_text = torch.optim.AdamW(lr=lr, params=text_model.parameters())
 
-    for batch_images, batch_recipe_titles in tqdm.tqdm(food_dataloader_batched):
+    losses = []
 
-        #batch_recipe_titles = list(batch_recipe_titles[0])
+    for epoch_num in range(100):
+        for batch_num, (batch_images, batch_recipe_titles) in enumerate(tqdm.tqdm(food_dataloader_batched)):
+            labels = torch.arange(batch_images.shape[0], device=d)
+            batch_images = batch_images.to(torch.float32).to(d)
 
-        text_latent = vision_model(batch_images)
-        img_latent = text_model(batch_recipe_titles)
+            text_latent = vision_model(batch_images)
+            img_latent = text_model(batch_recipe_titles)
+            text_latent = text_latent / torch.linalg.norm(text_latent, axis=1, keepdim=True)
+            img_latent = img_latent / torch.linalg.norm(img_latent, axis=1, keepdim=True)
 
-        text_latent = text_latent / torch.linalg.norm(text_latent, axis=1, keepdim=True)
-        img_latent = img_latent / torch.linalg.norm(img_latent, axis=1, keepdim=True)
+            logits = (text_latent @ img_latent.T) * torch.exp(text_model.t)
 
-        logits = (text_latent @ img_latent.T) * torch.exp(text_model.t)
+            loss_i = loss_fn(logits, labels)
+            loss_t = loss_fn(logits.T, labels)
+            loss = (loss_i + loss_t) / 2.0
 
-        loss_i = loss_fn(logits, labels)
-        loss_t = loss_fn(logits.T, labels)
-        loss = (loss_i + loss_t) / 2.0
+            opt_vision.zero_grad()
+            opt_text.zero_grad()
 
-        opt_vision.zero_grad()
-        opt_text.zero_grad()
+            loss.backward()
 
-        loss.backward()
+            opt_vision.step()
+            opt_text.step()
 
-        opt_vision.step()
-        opt_text.step()
+            losses.append(loss.detach())
+
+            if batch_num == 0:
+                print(f"\nacc of batch 0 of epoch {epoch_num}: "
+                      f"{((torch.argmax(logits, dim=0) == labels).sum() / batch_size).item():.3f}"
+                      f" (random is {1/batch_size:.3f})")
+
+        losses = losses[:-1] # final loss is kinda broken due to smaller batchsize
+
+        plt.plot(torch.stack(losses).cpu().numpy())
+        plt.show()
+    #print(loss.item())
 
 
 
 
 
-            # I_e = l2_normalize(np.dot(I_f, W_i), axis=1)
-            # T_e = l2_normalize(np.dot(T_f, W_t), axis=1)
-            # # scaled pairwise cosine similarities [n, n]
-            # logits = np.dot(I_e, T_e.T) * np.exp(t)
-            # # symmetric loss function
-            # labels = np.arange(n)
-            # loss_i = cross_entropy_loss(logits, labels, axis=0)
-            # loss_t = cross_entropy_loss(logits, labels, axis=1)
-            # loss = (loss_i + loss_t) / 2
+    # I_e = l2_normalize(np.dot(I_f, W_i), axis=1)
+    # T_e = l2_normalize(np.dot(T_f, W_t), axis=1)
+    # # scaled pairwise cosine similarities [n, n]
+    # logits = np.dot(I_e, T_e.T) * np.exp(t)
+    # # symmetric loss function
+    # labels = np.arange(n)
+    # loss_i = cross_entropy_loss(logits, labels, axis=0)
+    # loss_t = cross_entropy_loss(logits, labels, axis=1)
+    # loss = (loss_i + loss_t) / 2
 
-        #
-        # except Exception:
-        #     print("#NAME?.jpg fuckery")
+    #
+    # except Exception:
+    #     print("#NAME?.jpg fuckery")
 
 
 
